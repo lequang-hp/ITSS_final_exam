0ecad1c0ad5dc84aec57fd9bfe7ddfa863adeb15
lequang-hp <lequang0810@gmail.com>
2021-09-08 02:11:04 +0000

    add pagination

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,84 +1,22 @@
 import React from 'react';
 // import { useState }  from 'react';
-/* 
-  【Todoのデータ構成】
-　・key：Todoを特定するID（String）
-　・text：Todoの内容（String）
-　・done：完了状態（Boolean true:完了済み,, false:未完了）
-*/
 
-/* コンポーネント */
-import TodoItem from './TodoItem';
-import Input from './Input';
-import Filter from './Filter';
 
 /* カスタムフック */
-import useStorage from '../hooks/storage';
+import usePagination from '../hooks/pagination';
 
-/* ライブラリ */
-import {getKey} from "../lib/util";
 
 function Todo() {
-  const [items, putItems, clearItems] = useStorage();
-  // const [items, putItems] = React.useState([
-  //     /* テストコード 開始 */
-  //   { key: getKey(), text: '日本語の宿題', done: false },
-  //   { key: getKey(), text: 'reactを勉強する', done: false },
-  //   { key: getKey(), text: '明日の準備をする', done: false },
-  //   /* テストコード 終了 */
-  // ]);
-  
-  const [filter, setFilter] = React.useState('ALL');
-  // eslint-disable-next-line
-  const displayItems = items.filter(item => {
-    if (filter === 'ALL') return true;
-    if (filter === 'TODO') return !item.done;
-    if (filter === 'DONE') return item.done;
-  });
-  
-  const handleCheck = checked => {
-    const newItems = items.map(item => {
-      if (item.key === checked.key) {
-        item.done = !item.done;
-      }
-      return item;
-    });
-    putItems(newItems);
-  };
-  
-  const handleAdd = text => {
-    putItems([...items, { key: getKey(), text, done: false }]);
-  };
-  
-  const handleFilterChange = value => setFilter(value);
-  
+  const items = ["Huyen", "Hoa", "Hung", "Long"]
+  const { item, currentPage } = usePagination(items , 1);
   return (
-    <div className="panel">
-      <div className="panel-heading">
-        ITSS ToDoアプリ
-      </div>
-      <Input onAdd={handleAdd} />
-      <Filter
-        onChange={handleFilterChange}
-        value={filter}
-      />
-      {displayItems.map(item => (
-        <TodoItem 
-          key={item.key}
-          item={item}
-          onCheck={handleCheck}
-        />
-      ))}
-      <div className="panel-block">
-        {displayItems.length} items
-      </div>
-      <div className="panel-block">
-        <button className="button is-light is-fullwidth" onClick={clearItems}>
-          全てのToDoを削除
-        </button>
-      </div>
+    <div>
+      <p>学生一覧 { items }</p>
+      <p>位置：{currentPage}</p>
+      <p>名前: { item }</p>
+      <button>次に</button>
+      <button>前に</button>
     </div>
-    
   );
 }
 
